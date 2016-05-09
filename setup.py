c94670cb308a572e235f89580ae7a820c4d7ac00
Andrew Dorton <adorton@adobe.com>
2016-05-09 15:17:36 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(name='umapi',
+      version='0.2.0',
+      description='Adobe User Management API Interfaces',
+      url='git@git.corp.adobe.com:adorton/cce-umapi.git',
+      author='Andrew Dorton',
+      author_email='adorton@adobe.com',
+      license='Copyright Adobe Systems',
+      packages=['umapi'],
+      install_requires=[
+            'requests',
+            'cryptography',
+            'PyJWT',
+      ],
+      zip_safe=False)

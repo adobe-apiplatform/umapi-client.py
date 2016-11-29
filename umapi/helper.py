689553c665c4b5537e7453e5b9a51586fce71ff5
Daniel Brotsky <dev@brotsky.com>
2016-11-28 18:16:39 -0800

    Clean up helper.py.
    * Add an optional max_records arg to go with max_pages.
    * Bulletproof and clarify the logic about handling users vs. groups in the return dictionary.
    * Make the while loop actually count the attempts.
    * Make sure that a return 0 value for header Retry-After: doesn't cause an immediate retry.
    *Clean up the error messaging.
    * Fix the missing imports - it's hard to know how the code actually worked before.

--- umapi/helper.py
+++ umapi/helper.py
@@ -1,75 +1,83 @@
-import math
-import random
-import time
+from email.utils import parsedate_tz, mktime_tz
+from math import pow
+from random import randint
+from sys import maxsize
+from time import time, sleep
 from error import UMAPIError, UMAPIRetryError, UMAPIRequestError
 
-def paginate(call, org_id, max_pages=0):
-    page = 0
+def paginate(callable, org_id, max_pages=maxsize, max_records=maxsize):
+    """
+    Paginate through all results of a UMAPI query
+    :param callable: a query method from a UMAPI instance (callable as a function)
+    :param org_id: the organization being queried
+    :param max_pages: the max number of pages to collect before returning (default all)
+    :param max_records: the max number of records to collect before returning (default all)
+    :return: the queried records
+    """
+    page_count = 0
+    record_count = 0
     records = []
-    while True:
-        res = make_call(call, org_id, page)
-        if 'groups' in res:
-            res_type = 'groups'
-        else:
-            res_type = 'users'
-        records += res[res_type]
+    while page_count < max_pages and record_count < max_records:
+        res = make_call(callable, org_id, page_count)
+        page_count += 1
+        # the following incredibly ugly piece of code is very fragile.
+        # the problem is that we are a "dumb helper" that doesn't understand
+        # the semantics of the UMAPI or know which callable we were given.
+        # TODO: make this better, probably by being smart about the callable
+        if "groups" in res:
+            records += res["groups"]
+        elif "users" in res:
+            records += res["users"]
+        record_count = len(records)
+        if res.get("lastPage"):
+            break
+    return records
 
-        page += 1
-        if max_pages and max_pages >= page:
-            return records
 
-        if 'lastPage' in res and res['lastPage']:
-            return records
-
-# Copy & hack retry code from aedash_connector/connector.py
-def make_call(call, org_id, page):
+def make_call(callable, org_id, page):
+    """
+    Make a single UMAPI call with error handling and server-controlled throttling.
+    (Adapted from sample code at https://www.adobe.io/products/usermanagement/docs/samples#retry)
+    :param callable: a query method from a UMAPI instance (callable as a function)
+    :param org_id: the organization being queried
+    :param page: the page number of the desired result set
+    :return: the json (dictionary) received from the server (if any)
+    """
+    wait_time = 0
     num_attempts = 0
     num_attempts_max = 4
     backoff_exponential_factor = 15  # seconds
     backoff_random_delay_max = 5  # seconds
 
-    while True:
-        num_attempts += 1
-
-        if num_attempts > num_attempts_max:
-            print("ACTION FAILURE NO MORE RETRIES, SKIPPING...")
-            break
-
+    while num_attempts < num_attempts_max:
+        if wait_time > 0:
+            sleep(wait_time)
+            wait_time = 0
         try:
-            res = call(org_id, page)
-            return res
-        except UMAPIRequestError as e:
-            print("PAGINATE ERROR - %s", e.code)
-            break
+            num_attempts += 1
+            return callable(org_id, page)
         except UMAPIRetryError as e:
-            print("PAGINATE FAILURE -- %s - RETRYING", e.res.status_code)
+            print("UMAPI service temporarily unavailable (attempt %d) -- %s", num_attempts, e.res.status_code)
             if "Retry-After" in e.res.headers:
-                retry_after_date = email.utils.parsedate_tz(e.res.headers["Retry-After"])
-                if retry_after_date is not None:
+                advice = e.res.headers["Retry-After"]
+                advised_time = parsedate_tz(advice)
+                if advised_time is not None:
                     # header contains date
-                    time_backoff = int(email.utils.mktime_tz(retry_after_date) - time.time())
+                    wait_time = int(mktime_tz(advised_time) - time())
                 else:
                     # header contains delta seconds
-                    time_backoff = int(e.res.headers["Retry-After"])
-            else:
-                # use exponential backoff with random delayh
-                time_backoff = int(math.pow(2, num_attempts - 1)) * \
-                               backoff_exponential_factor + \
-                               random.randint(0, backoff_random_delay_max)
-
-            print("Retrying in " + str(time_backoff) + " seconds...")
-            time.sleep(time_backoff)
+                    wait_time = int(advice)
+            if wait_time <= 0:
+                # use exponential back-off with random delay
+                delay = randint(0, backoff_random_delay_max)
+                wait_time = (int(pow(2, num_attempts)) * backoff_exponential_factor) + delay
+            print("Next retry in %d seconds...", wait_time)
             continue
+        except UMAPIRequestError as e:
+            print("UMAPI error processing request -- %s", e.code)
+            return {}
         except UMAPIError as e:
-            print("PAGINATE ERROR -- " + e.res.status_code  + " - " + e.res.text)
-            break
-
-        if res['result'] == 'success': # and not res['notCompleted']:
-            print('PAGINATE SUCCESS -- completed') #  + res['completed'])
-            break
-        elif res['result'] == 'success':
-            print('PAGINATE PARTIAL SUCCESS') # -- %d completed, %d failed', res['completed'], res['incomplete'])
-            break
-        else:
-            print("PAGINATE FAILURE")
-            break
+            print("HTTP error processing request -- %s: %s", e.res.status_code, e.res.text)
+            return {}
+    print("UMAPI timeout...giving up on results page %d after %d attempts.", page, num_attempts_max)
+    return {}

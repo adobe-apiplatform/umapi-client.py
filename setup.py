9775ef0e5d3b491ca046ae8bb8b2901927ff33fe
Daniel Brotsky <dbrotsky@adobe.com>
2016-12-12 23:53:14 -0800

    Update the setup.py metadata to be clearer, rev to v1.0.1.

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 
 setup(name='adobe-umapi',
-      version='1.0.0',
-      description='Adobe UMAPI client - see http://bit.ly/2hwkVrs',
+      version='1.0.1',
+      description='Adobe User Management API (UMAPI) client - see http://bit.ly/2hwkVrs',
       url='https://github.com/adobe-apiplatform/adobe-umapi.py',
       author='Daniel Brotsky',
       author_email='dbrotsky@adobe.com',
-      license='Copyright (c) 2016 Adobe Systems Incorporated.  All rights reserved.',
+      license='Copyright (c) 2016 Adobe Systems Incorporated. All rights reserved. Distributed under the MIT license.',
       packages=find_packages(),
       install_requires=[
         'requests',
         'cryptography',
         'PyJWT',
+        'six',
       ],
       setup_requires=[
         'pytest-runner',
@@ -20,6 +21,5 @@ setup(name='adobe-umapi',
       tests_require=[
         'pytest',
         'mock',
-        'six',
       ],
       zip_safe=False)
